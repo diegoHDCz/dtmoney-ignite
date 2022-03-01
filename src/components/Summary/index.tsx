48fe42cd01dd89cb925db0574811cefa994f6063
Diego Czajka <67860687+diegoHDCz@users.noreply.github.com>
2022-03-01 18:28:43 -0300

    calculating dashboard

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,14 +1,29 @@
-import React, { useContext } from 'react'
+import React from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
-import { TransactionsContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 import { Container } from "./styles";
 
 
 export function Summary () {
-  const transactions = useContext(TransactionsContext)
+  const { transactions } = useTransactions()
 
+  const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+      acc.deposits += transaction.amount
+      acc.total += transaction.amount
+    } else {
+      acc.withdraws += transaction.amount
+      acc.total -= transaction.amount
+    }
+    return acc
+
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
 
   return (
     <Container>
@@ -19,7 +34,13 @@ export function Summary () {
           <img src={incomeImg} alt="Entradas" />
         </header>
         <strong>
-          R$1000,00
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(
+            summary.deposits
+          )}
+
         </strong>
       </div>
 
@@ -29,7 +50,11 @@ export function Summary () {
           <img src={outcomeImg} alt="Saídas" />
         </header>
         <strong>
-          - R$500,00
+          -{
+            new Intl.NumberFormat('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(summary.withdraws)}
         </strong>
       </div>
 
@@ -39,7 +64,10 @@ export function Summary () {
           <img src={totalImg} alt="Total" />
         </header>
         <strong>
-          R$500,00
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(summary.total)}
         </strong>
       </div>
     </Container>
--- src/components/TransactionsTable/intex.tsx
+++ src/components/TransactionsTable/intex.tsx
@@ -1,9 +1,8 @@
-import { useContext } from "react";
-import { TransactionsContext } from "../../TransactionsContext";
+import { useTransactions } from "../../hooks/useTransactions";
 import { Container } from "./styles";
 
 export function TransactionsTable () {
-  const transactions = useContext(TransactionsContext)
+  const { transactions } = useTransactions()
 
   return (
     <Container>
@@ -21,7 +20,7 @@ export function TransactionsTable () {
 
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(transaction.amount)}</td>
+              <td className={transaction.type}>{(transaction.type === 'withdraw' ? '- ' : '') + new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(transaction.amount)}</td>
               <td>{transaction.category}</td>
               <td>
                 {new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
